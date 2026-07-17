e499166c554cb3aff8df47eb98f34cf3f1e8ecaf
realnureddin <realnureddin@gmail.com>
2026-07-17 15:37:15 +0300

    fix requirements for vercel

--- alembic/env.py
+++ alembic/env.py
@@ -10,11 +10,13 @@ from app.config import settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option(
-    "sqlalchemy.url", f"postgresql+psycopg2://{settings.database_username}:{settings.
-    database_password}@{settings.database_hostname}:{settings.
-                                     database_port}/{settings.database_name}")
+# config.set_main_option(
+#     "sqlalchemy.url", f"postgresql+psycopg2://{settings.database_username}:{settings.
+#     database_password}@{settings.database_hostname}:{settings.
+#                                      database_port}/{settings.database_name}")
+database_url = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}?sslmode=require"
 
+config.set_main_option("sqlalchemy.url", database_url)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
--- app/database.py
+++ app/database.py
@@ -7,8 +7,7 @@ from sqlalchemy.orm import sessionmaker, Session
 from .config import settings
 
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
-
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}?sslmode=require"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
